5afa357b00b8f1b6b013d25f069093bf4e248e0e
Helen Lehrer <hlehrer@outlook.com>
2022-08-19 10:07:37 -0700

    Add 2nd TDD to convert to string

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Test: "It should return an array of numbers from 0 to the user's inputted number
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
+Describe: convertToString()
 
-Describe: replaceDigits()
-Test: "It should replace any digit that containes a 1 with "Beep!"" 
+Test: "It should convert every number in the array to strings."
+Code: convertToString([0, 1, 2, 3, 4, 5]);
+Expected Output: ["0", "1", "2", "3", "4", "5"]
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-//Business Logic
-function beepBoop(number) {
+const userInput= 5;
 let arrayCount = [];
-for (let i = 0; i <= number; i++) {
+function beepBoop(userInput) {
+for (let i = 0; i <= userInput; i++) {
   arrayCount.push(i);
+};
+};
+let stringArray = arrayCount;
+function convertToString () {
+  stringArray = arrayCount.map(function(number) {
+  return String(number);
+});
+};
+
+/*
+function replaceDigits() {
+arrayCount.toString();
+arrayCount.forEach(function(number) {
+  if (number )
+})
 }
-}
+*/
