6f67e9d73683df423a753edb0c1ea1249403e175
Helen Lehrer <hlehrer@outlook.com>
2022-08-19 13:08:34 -0700

    WIP replaceDigits()

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,47 @@ function convertToString () {
 });
 };
 
+const substring1 = '1';
+const substring2 = '2';
+const substring3 = '3';
 let replacedArray = stringArray;
+function replaceArray() {
+replacedArray.forEach(function (number) {
+  if (number.includes(substring1)) {
+    return true;
+  } else if (number.includes(substring2)) {
+    return true;
+  } else if (number.includes(substring3)) {
+    return true;
+  }
+  });
+}
+/*
+const match = stringArray.find(function(number) {
+  if 
+})
+
+let replacedArray = stringArray;
+let newArray = [];
 function replaceDigits() {
+  replacedArray.forEach(function (number) {
+    if (number.includes("3")) {
+      number = "Won't you be my neighbor?";
+    } else if (number.includes("2")) {
+      number.replace("Boop!");
+    } else if (number.includes("1")) {
+      number.replace("Beep!");
+    }
+  });
+}
+*/
+  /*
   if (replacedArray.includeas("1")) {
     replacedArray.splice()
   }
   beepedArray = stringArray.replaceAll("1", "'Beep!'").replaceAll("2", "'Boop!'").replaceAll("3", "'Won't you be my neighbor?'");
   return beepedArray;
-}
+  */
 
 /*
 let joinedString = stringArray;
