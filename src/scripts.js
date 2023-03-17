0aad5a4a0a69a712a1df24910a7f4f81e7ebb4de
Helen Lehrer <hlehrer@outlook.com>
2023-03-16 21:17:05 -0700

    Delete comments

--- src/scripts.js
+++ src/scripts.js
@@ -13,24 +13,3 @@ export default function countUp(userInput) {
   };
   return outputArray;
 }
-
-
-
-/*export default function countUp(userInput) {
-let outputArray = [];
-  return outputArray;
-
-  let outputArray = [];
-  for (let i = 0; i <= userInput; i++) {
-    if (i.toString().includes('3')) {
-      outputArray.push("Won't you be my neighbor?");
-    } else if (i.toString().includes('2')) {
-      outputArray.push("Boop!");
-    } else if (i.toString().includes('1')) {
-      outputArray.push("Beep!");
-    } else {
-      outputArray.push(i);
-    }
-  };
-  return outputArray;
-}*/
